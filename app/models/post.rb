e5ea15fd8808a376cf02ef92d42702618bafc428
David Wise <dhmwise@hotmail.com>
2014-06-03 17:11:04 +0200

    :path option is mandatory for S3 storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-  :default_url => "/images/:style/missing.png"
+                    :path => ':class/:attachment/:id/:style/:filename'
+
    validates_attachment_content_type :image, :content_type => /\Aimage/
 end
 
